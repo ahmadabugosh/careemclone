f2b38264cf6f1c901a2031e56dedabaad9f6a092
Ahmad Abugosh <ahmadabugosh@gmail.com>
2017-04-13 16:44:38 +0400

    Added test for driver model and post request

--- app.js
+++ app.js
@@ -1,7 +1,13 @@
 const express = require('express');
 const routes = require('./routes/routes');
+const mongoose= require('mongoose');
+const bodyParser= require('body-parser');
 
 const app = express();
+mongoose.Promise=global.Promise;
+mongoose.connect('mongodb://localhost/muber');
+
+app.use(bodyParser.json());
 
 routes(app);
 
