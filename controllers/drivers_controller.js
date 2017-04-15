4d34ac11bbe3bc88258f12a34872766d75c96c56
Ahmad Abugosh <ahmadabugosh@gmail.com>
2017-04-15 22:55:34 +0400

    Finished driver create test

--- controllers/drivers_controller.js
+++ controllers/drivers_controller.js
@@ -11,8 +11,10 @@ res.send({hi:'there'});
 create(req, res)
 
 {
-console.log(req.body);
-res.send({hi:'there'});
+const driverProps = req.body;
+Driver.create(driverProps)
+.then(driver=> res.send(driver));
+
 
 }
 
